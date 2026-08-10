d06537fc9fde921a69bf3235e590ca3b398bb6d6
EdenAlpha <elijah.emmanuel@physci.uniben.edu>
2026-08-10 12:18:45 +0100

    Add exact TAR locator

---
+++ paqrep/tar_transform.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+import io, tarfile
+
+def parse(data):
+    try:
+        out=[]
+        with tarfile.open(fileobj=io.BytesIO(data), mode='r:*') as tf:
+            for m in tf.getmembers():
+                if not m.isfile() or m.size < 0: continue
+                ds=int(m.offset_data); sz=int(m.size)
+                if ds < 0 or ds+sz > len(data): return None
+                out.append((ds,sz,m.name.encode('utf-8','surrogateescape')))
+        return out
+    except Exception:
+        return None
