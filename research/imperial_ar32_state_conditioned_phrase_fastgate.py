653e0f2ed414b4d8bac642e94d3500b5aefefe1d
EdenAlpha <elijah.emmanuel@physci.uniben.edu>
2026-08-13 01:15:46 +0100

    Add 16-channel fast gate for state-conditioned phrase covering

---
+++ research/imperial_ar32_state_conditioned_phrase_fastgate.py
@@ -0,0 +1,9 @@
+import sys
+import imperial_ar32_state_conditioned_phrase_cover as m
+m.C=16
+m.C0=512
+m.DICT_END=3072
+m.END=5120
+m.LENS=(4,8)
+m.SIZES=(64,256)
+if __name__=='__main__':m.main(sys.argv[1])
