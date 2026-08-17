77df485e056e595ff01df12e81eedd95777eb8f7
EdenAlpha <elijah.emmanuel@physci.uniben.edu>
2026-08-17 20:02:01 +0100

    Wire causal context entropy into native migrated 3D screen

---
+++ research/migrated_volume_3d_runner_context.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+import migrated_volume_3d_codec as codec
+import migrated_volume_3d_context_entropy as context_entropy
+codec.pack=context_entropy.pack
+codec.unpack=context_entropy.unpack
+import migrated_volume_3d_runner as runner
+import migrated_volume_3d_geometry_v2 as geometry_v2
+runner.SCAN=40000
+runner.NY=4
+runner.NX=32
+_base_geometry_candidates=runner.geometry_candidates
+runner.geometry_candidates=lambda A: geometry_v2.geometry_candidates(_base_geometry_candidates,runner,A)
+if __name__=='__main__':
+ context_entropy.sanity()
+ runner.main()
