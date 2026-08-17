2e089db76869c6ff178a6b89f4766fc48407a51c
EdenAlpha <elijah.emmanuel@physci.uniben.edu>
2026-08-17 21:03:09 +0100

    Wire compact PPM block menu v9 into native 3D screen

---
+++ research/migrated_volume_3d_runner_v9.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Native-geometry migrated-volume screen with compact PPM block menu v9."""
+import migrated_volume_3d_runner_v8 as v8runner
+import migrated_volume_3d_ppm_blocks_v9 as v9
+v9.install()
+if __name__=='__main__':
+    v9.sanity()
+    v8runner.v7runner.v6runner.v5runner.v4runner.v3.v2.runner.main()
