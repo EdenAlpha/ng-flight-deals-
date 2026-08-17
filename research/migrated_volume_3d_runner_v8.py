cf2a6a95973303a9ac467350d04bc5a11dc2c7fd
EdenAlpha <elijah.emmanuel@physci.uniben.edu>
2026-08-17 20:49:51 +0100

    Wire context side-permutation v8 into native 3D screen

---
+++ research/migrated_volume_3d_runner_v8.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Native-geometry migrated-volume screen with side-map traversal v8."""
+import migrated_volume_3d_runner_v7 as v7runner
+import migrated_volume_3d_context_sideperm_v8 as v8
+v8.install()
+if __name__=='__main__':
+    v8.sanity()
+    v7runner.v6runner.v5runner.v4runner.v3.v2.runner.main()
