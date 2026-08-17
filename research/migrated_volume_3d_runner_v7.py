3a6a189337d28be6ebc126a6f7ee1ec8a224b82d
EdenAlpha <elijah.emmanuel@physci.uniben.edu>
2026-08-17 20:44:35 +0100

    Wire online context v7 into native 3D screen

---
+++ research/migrated_volume_3d_runner_v7.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Native-geometry migrated-volume screen with online context v7."""
+import migrated_volume_3d_runner_v6 as v6runner
+import migrated_volume_3d_adaptive_context_v7 as v7
+v7.install()
+if __name__=='__main__':
+    v7.sanity()
+    v6runner.v5runner.v4runner.v3.v2.runner.main()
