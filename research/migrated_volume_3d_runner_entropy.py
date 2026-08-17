e96ec41c92fa09fdf12216fc840952fe3cf3b728
EdenAlpha <elijah.emmanuel@physci.uniben.edu>
2026-08-17 19:49:42 +0100

    Freeze base transform table when competing blockwise 3D codecs

--- research/migrated_volume_3d_runner_entropy.py
+++ research/migrated_volume_3d_runner_entropy.py
@@ -7,10 +7,20 @@ codec.unpack=entropy.unpack
 import migrated_volume_3d_block_fix as block_fix
 block=block_fix.install()
 _base_compete=codec.compete
+_base_names=dict(codec.NAMES)
+
+def _frozen_base_compete(X,eps):
+    full=codec.NAMES
+    codec.NAMES=_base_names
+    try:
+        return _base_compete(X,eps)
+    finally:
+        codec.NAMES=full
+
 codec.NAMES[1064]='block_causal_b64'
 codec.NAMES[1128]='block_causal_b128'
 codec.NAMES[1256]='block_causal_b256'
-codec.compete=lambda X,eps: block.compete_with_base(X,eps,_base_compete)
+codec.compete=lambda X,eps: block.compete_with_base(X,eps,_frozen_base_compete)
 
 import migrated_volume_3d_runner as runner
 import migrated_volume_3d_geometry_v2 as geometry_v2
@@ -19,6 +29,6 @@ _base_geometry_candidates=runner.geometry_candidates
 runner.geometry_candidates=lambda A: geometry_v2.geometry_candidates(_base_geometry_candidates,runner,A)
 
 if __name__=='__main__':
- entropy.sanity()
- block.sanity()
- runner.main()
+    entropy.sanity()
+    block.sanity()
+    runner.main()
